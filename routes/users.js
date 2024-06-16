e37feefc32822ceae24ccb2da7725d4efb437eeb
RizWaaN3024 <rizwaanans786@gmail.com>
2024-06-16 21:52:37 +0530

    Added post method and uuid support

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,27 @@
 import express from "express";
+import { v4 as uuidv4 } from 'uuid';
+
 
 // using router
 const router = express.Router();
 
-const users = [
-    {
-        "firstName": "John",
-        "lastName": "Doe",
-        "age": 25
-    }
-]
+const users = [];
 
 // all routes here are starting with /users 
 router.get('/', (req, res) => {
-    res.send("Hello From userRoute")
+    res.send(users)
+})
+
+router.post("/", (req, res) => {
+    const user = req.body;
+
+    const userId = uuidv4();
+
+    const userWithId = {...user, id: userId};
+
+    users.push(userWithId);
+    res.send(`User with the name ${user.firstName} added to the database`)
+
 })
 
 export default router;
